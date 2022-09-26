5b52a90b739545f829b71efe9ee4023f5f2c42ff
sergejsaenko <ssaenko@live.de>
2022-09-26 20:35:15 +0200

    corrects

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const navSlide=()=> {
         //Animate Links
         navLinks.forEach((link,index)=>{
             if(link.style.animation){
-                link.style.animation=""
+                link.style.animation="";
             } else{
                 link.style.animation=`navLinkFade 0.5s ease forwards ${index/7+1.5}s`;
             }
