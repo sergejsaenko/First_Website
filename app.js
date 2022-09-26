f1a84d1e64acceeac9d28ae27704dbf50cdc725a
sergejsaenko <ssaenko@live.de>
2022-09-26 20:55:37 +0200

    found issue with burger not working

--- app.js
+++ app.js
@@ -1,26 +1,24 @@
-const navSlide=()=> {
-    const burger=document.querySelector(".burger");
-    const nav=document.querySelector(".nav-links");
-    const navLinks=document.querySelectorAll(".nav-links li");
-    
-    burger.addEventListener("click", ()=>{
-        //Toggle Nav
-        nav.classList.toggle(".nav-active");
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelectorAll(".nav-links li");
 
-        //Animate Links
-        navLinks.forEach((link,index)=>{
-            if(link.style.animation){
-                link.style.animation="";
-            } else{
-                link.style.animation=`navLinkFade 0.5s ease forwards ${index/7+1.5}s`;
-            }
-        });
+  burger.addEventListener("click", () => {
+    //Toggle Nav
+    nav.classList.toggle("nav-active");
 
-        //Burger Animation
-        burger.classList.toggle(".toggle");
+    //Animate Links
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+      }
     });
-}
 
-const app=()=>{
-    navSlide();
-}
+    //Burger Animation
+    burger.classList.toggle("toggle");
+  });
+};
+
+navSlide();
